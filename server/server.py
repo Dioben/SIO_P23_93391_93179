5c7ca7b6797820c40b3495c685020951910eedc7
p-laranjinha <plcasimiro@ua.pt>
2020-12-23 17:41:00 +0000

    Better variable names and changed protocol choice Still needs more comments Auth part not done

--- server/server.py
+++ server/server.py
@@ -15,32 +15,13 @@ from cryptography.hazmat.primitives.asymmetric import padding as asympad
 import uuid
 from time import time
 from cryptography import x509
+import cipher_suites
 
 logger = logging.getLogger('root')
 FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
 logging.basicConfig(format=FORMAT)
 logger.setLevel(logging.DEBUG)
 
-HOUR = 3600
-DAY = 24*3600
-
-with open("127.0.0.1.crt","rb") as cert:
-    certpem = cert.read()
-    SELF_CERTIFICATE = x509.load_pem_x509_certificate(certpem)
-with open('privkey.pem','rb') as keyfile:
-    SELF_PRIVATE_KEY = serialization.load_pem_private_key(keyfile.read(),password=None)
-
-cipherposs = {'AES-256':ciphers.algorithms.AES,'Camellia-256':ciphers.algorithms.Camellia}
-modeposs = {'CBC':ciphers.modes.CBC,'CFB':ciphers.modes.CFB,'OFB':ciphers.modes.OFB}
-digests = {'SHA-256':hashes.SHA256,'SHA3-256':hashes.SHA3_256}
-
-
-HASHES={0:hashes.SHA256,1:hashes.SHA3_256}
-CIPHERS={0:'AES-256',1:'Camellia-256'}         
-MODES = {0:'CBC',1:'CFB',2:'OFB'}
-
-keys={}
-licenses = {}
 CATALOG = { '898a08080d1840793122b7e118b27a95d117ebce': 
             {
                 'name': 'Sunny Afternoon - Upbeat Ukulele Background Music',
@@ -55,65 +36,103 @@ CATALOG = { '898a08080d1840793122b7e118b27a95d117ebce':
 CATALOG_BASE = 'catalog'
 CHUNK_SIZE = 1024 * 4
 
+
+HOUR = 3600
+DAY = 24*3600
+
+with open("127.0.0.1.crt","rb") as cert:
+    SERVER_PEM_CERTIFICATE = cert.read()
+    SERVER_CERTIFICATE = x509.load_pem_x509_certificate(SERVER_PEM_CERTIFICATE)
+with open('privkey.pem','rb') as keyfile:
+    SERVER_PRIVATE_KEY = serialization.load_pem_private_key(keyfile.read(),password=None)
+
+cipherposs = {'AES-256':ciphers.algorithms.AES,'Camellia-256':ciphers.algorithms.Camellia}
+modeposs = {'CBC':ciphers.modes.CBC,'CFB':ciphers.modes.CFB,'OFB':ciphers.modes.OFB}
+digests = {'SHA-256':hashes.SHA256,'SHA3-256':hashes.SHA3_256}
+
+
+HASHES={0:hashes.SHA256,1:hashes.SHA3_256}
+CIPHERS={0:'AES-256',1:'Camellia-256'}         
+MODES = {0:'CBC',1:'CFB',2:'OFB'}
+
+keys={}
+licenses = {}
+
 class MediaServer(resource.Resource):
     isLeaf = True
 
-
-    def do_key_set(self,request):
-        encoding = request.getHeader(b'hashmode')[0]
-        peer_public_key= request.content.read()
-        salt = peer_public_key[0:32]
-        peer_public_key = peer_public_key[32::]
-
-        peer_public_key = serialization.load_pem_public_key(peer_public_key)
-        private_key = ec.generate_private_key(ec.SECP384R1())
-        sendable_public_key = private_key.public_key()
-        shared_key = private_key.exchange(ec.ECDH(), peer_public_key)
-        derived_key = HKDF(algorithm= HASHES[encoding](),length=32,salt=salt,info=None).derive(shared_key)
-        
-        user = uuid.uuid4().hex
-        keys[(user).encode('latin')]= (derived_key,time()+DAY)
-        
-        
-        return (user+"\n").encode('latin')+sendable_public_key.public_bytes(encoding=serialization.Encoding.PEM,format=serialization.PublicFormat.SubjectPublicKeyInfo)
-
-
-    def do_get_protocols(self,request):
+    def do_protocols(self,request):
+        """ Gets client random and a list of possible protocols/cipher suite
+            Chooses from one of the protocols
+            Returns the server certificate, the chosen protocol and the signed random"""
         data = request.content.read()
-        secret = data[0:32]
-        protocolmap = json.loads(data[32::])
-        protocol = 'TLS_ECHDE_RSA_'
-        ciphers = [x for x in protocolmap['ciphers'] if x in cipherposs.keys()]
-        if ciphers ==[]:
-            protocol='No available ciphers'
-        digestposs = [x for x in protocolmap['digests'] if x in digests.keys()]
-        if digestposs==[]:
-            protocol='No available digests'
-        modes = [x for x in protocolmap['modes'] if x in modeposs.keys()]
-        if modes==[]:
-            protocol='No available modes'
-        cipherchoice = choice(ciphers)
-        protocol+="WITH_"+cipherchoice+"_"
-        modechoice = choice(modes)
-        protocol+=modechoice+"_"
-        digestchoice = choice(digestposs)
-        
-        if digestchoice =='SHA-256':
-            hashf = hashes.SHA256
-        elif digestchoice=='SHA3-256':
-            hashf = hashes.SHA3_256
-        
-        protocol+=digestchoice+"\n"
-
-        encrypted_secret = SELF_PRIVATE_KEY.sign(
-            secret,
+        client_random = data[0:32]
+        protocol_list = json.loads(data[32::])
+        # Checks if all protocol possibilities are valid
+        protocol_list = [p for p in protocol_list if p.split('_')[3] in cipher_suites.cipher_possibilities and p.split('_')[4] in cipher_suites.mode_posibilities and p.split('_')[5] in cipher_suites.hash_possibilities]
+        if protocol_list == []:
+            protocol = 'No available protocol'
+        else:
+            protocol = choice(protocol_list)
+        HASH = cipher_suites.HASHES[cipher_suites.cs_indexes[protocol.split('_')[5]]]
+        signed_client_random = SERVER_PRIVATE_KEY.sign(
+            client_random,
             asympad.PSS(
-                mgf= asympad.MGF1(hashf()),
+                mgf= asympad.MGF1(HASH()),
                 salt_length=asympad.PSS.MAX_LENGTH
                 ),
-                hashf())
-        msg = protocol.encode('latin')+certpem+encrypted_secret
-        return msg
+                HASH())
+        return (protocol+'\n').encode('latin') + SERVER_PEM_CERTIFICATE + signed_client_random
+
+
+    def do_key(self,request):
+        """ Gets salt (random bytes) and the client DH parameter (key used to find the shared key)
+            Calculates shared key and saves the userId and its time
+            Returns userId and the server DH parameter"""
+        HASH = cipher_suites.HASHES[request.getHeader(b'suite_hash')[0]]
+        data = request.content.read()
+        salt = data[0:32]
+        client_dh = data[32::]
+
+        client_dh = serialization.load_pem_public_key(client_dh)
+        server_dh_private = ec.generate_private_key(ec.SECP384R1())
+        server_dh = server_dh_private.public_key()
+        shared_key = server_dh_private.exchange(ec.ECDH(), client_dh)
+        derived_key = HKDF(algorithm= HASH(),length=32,salt=salt,info=None).derive(shared_key)
+        
+        clientID = uuid.uuid4().hex
+        keys[(clientID).encode('latin')]= (derived_key,time()+DAY)
+        
+        return (clientID+"\n").encode('latin')+server_dh.public_bytes(encoding=serialization.Encoding.PEM,format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+
+    def do_auth(self,request):
+        if request.getHeader(b'ID') not in keys.keys():
+            return "register a key first".encode('latin')
+        if keys[request.getHeader(b'ID')][1]<time():
+            try:
+                del keys[request.getHeader(b'ID')]
+            except:
+                pass
+            request.setResponseCode(401)
+            return "your key has expired".encode('latin')
+        data = request.content.read()
+        iv = data[0:16]
+        CIPHER = cipher_suites.CIPHERS[request.getHeader(b'suite_cipher')[0]]
+        MODE = cipher_suites.MODES[request.getHeader(b'suite_mode')[0]]
+        HASH = cipher_suites.HASHES[request.getHeader(b'suite_hash')[0]]
+        key = keys[request.getHeader(b'id')][0]
+        unpadder = padder = padding.PKCS7(256).unpadder()
+        decryptor = ciphers.Cipher(CIPHER(key), MODE(iv)).decryptor()
+        cert = decryptor.update(data[16:-384])+decryptor.finalize()
+        cert = unpadder.update(cert)+unpadder.finalize()
+        cert = x509.load_pem_x509_certificate(cert)
+        sig = data[-384:]
+        cert.public_key().verify(sig,request.getHeader(b'ID'),asympad.PKCS1v15(),hashes.SHA256())
+        licensekey = os.urandom(256) 
+        licenses[request.getHeader(b'ID')]=(cert.public_key(),key,time()+HOUR) #this should  be PUBLIC KEY - LICENSE - EXPIRE TIME
+        return key
+
 
     # Send the list of media files to clients
     def do_list(self, request):
@@ -210,65 +229,12 @@ class MediaServer(resource.Resource):
         request.responseHeaders.addRawHeader(b"content-type", b"application/json")
         return json.dumps({'error': 'unknown'}, indent=4).encode('latin')
 
-    
-    
-    
-    
-  
-
-    def try_auth(self,request):
-        if request.getHeader(b'ID') not in keys.keys():
-            return "register a key first".encode('latin')
-        if keys[request.getHeader(b'ID')][1]<time():
-            try:
-                del keys[request.getHeader(b'ID')]
-            except:
-                pass
-            request.setResponseCode(401)
-            return "your key has expired".encode('latin')
-        data = request.content.read()
-        iv = data[0:16]
-        algo= CIPHERS[request.getHeader(b'ciphermode')[0]]
-        digest = HASHES[request.getHeader(b'hashmode')[0]]
-        mode = MODES[request.getHeader(b'modemode')[0]]
-        key = keys[request.getHeader(b'id')][0]
-        unpadder = padder = padding.PKCS7(256).unpadder()
-        decryptor = ciphers.Cipher(cipherposs[algo](key),modeposs[mode](iv)).decryptor()
-        cert = decryptor.update(data[16:-384])+decryptor.finalize()
-        cert = unpadder.update(cert)+unpadder.finalize()
-        cert = x509.load_pem_x509_certificate(cert)
-        sig = data[-384:]
-        cert.public_key().verify(sig,request.getHeader(b'ID'),asympad.PKCS1v15(),hashes.SHA256())
-        licensekey = os.urandom(256) 
-        licenses[request.getHeader(b'ID')]=(cert.public_key(),key,time()+HOUR) #this should  be PUBLIC KEY - LICENSE - EXPIRE TIME
-        return key
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   # Handle a GET request
     def render_GET(self, request):
         logger.debug(f'Received request for {request.uri}')
-
         try:
             if request.path == b'/api/list':
                 return self.do_list(request)
-
             elif request.path == b'/api/download':
                 return self.do_download(request)
             else:
@@ -285,16 +251,13 @@ class MediaServer(resource.Resource):
     def render_POST(self, request):
         logger.debug(f'Received POST for {request.uri}')
         if request.path==b'/api/key':
-            return self.do_key_set(request)
+            return self.do_key(request)
         elif request.path == b'/api/auth':
-            return self.try_auth(request)
+            return self.do_auth(request)
         elif request.path == b'/api/protocols':
-                return self.do_get_protocols(request)
+            return self.do_protocols(request)
         request.setResponseCode(501)
         return b''
-
-
-
 
 
 
